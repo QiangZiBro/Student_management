8eec8517464df96df793f4bb58e3bacb2d05ed8f
QiangZiBro <qiangzibro@gmail.com>
2020-03-27 01:15:43 +0800

    数据库

--- management/admin.py
+++ management/admin.py
@@ -2,24 +2,91 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Student, Lesson, Teacher, Score
+from .models import Student, Lesson, Teacher, Score, Teaching, Account
 
 # Register your models here.
 # admin.site.register(Student)
-admin.site.register(Lesson)
-admin.site.register(Teacher)
-admin.site.register(Score)
 
+
+
+@admin.register(Account)
+class StudentAdmin(admin.ModelAdmin):
+    # 要显示的字段
+    list_display = ('a_student', 'a_password')
+
+    # 需要搜索的字段
+    search_fields = ('s_name',)
+
+    # 分页显示，一页的数量
+    list_per_page = 10
+
+    actions_on_top = True
 
 # Register your models here.
 # Example
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     # 要显示的字段
-    list_display = ('s_name', 'sex')
+    list_display = ('s_name', 's_gender')
 
     # 需要搜索的字段
     search_fields = ('s_name',)
+
+    # 分页显示，一页的数量
+    list_per_page = 10
+
+    actions_on_top = True
+
+
+@admin.register(Lesson)
+class StudentAdmin(admin.ModelAdmin):
+    # 要显示的字段
+    list_display = ('l_name',)
+
+    # 需要搜索的字段
+    search_fields = ('l_name',)
+
+    # 分页显示，一页的数量
+    list_per_page = 10
+
+    actions_on_top = True
+
+
+@admin.register(Teacher)
+class StudentAdmin(admin.ModelAdmin):
+    # 要显示的字段
+    list_display = ('t_name', 't_gender')
+
+    # 需要搜索的字段
+    search_fields = ('t_name',)
+
+    # 分页显示，一页的数量
+    list_per_page = 10
+
+    actions_on_top = True
+
+
+@admin.register(Score)
+class StudentAdmin(admin.ModelAdmin):
+    # 要显示的字段
+    list_display = ('s_student', 's_lesson', 's_score')
+
+    # 需要搜索的字段
+    search_fields = ('s_student', 's_lesson')
+
+    # 分页显示，一页的数量
+    list_per_page = 10
+
+    actions_on_top = True
+
+
+@admin.register(Teaching)
+class StudentAdmin(admin.ModelAdmin):
+    # 要显示的字段
+    list_display = ('t_teacher', 't_lesson')
+
+    # 需要搜索的字段
+    search_fields = ('t_teacher',)
 
     # 分页显示，一页的数量
     list_per_page = 10
--- management/models.py
+++ management/models.py
@@ -6,12 +6,11 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    s_number = models.IntegerField(primary_key=True)
-    s_name = models.CharField(max_length=20,verbose_name='姓名', help_text="学生姓名")
-    sex = models.CharField(max_length=2, verbose_name='性别')
-    subject = models.CharField(max_length=20)
-    grade = models.CharField(max_length=20)
-    ID_number = models.IntegerField()
+    s_number = models.AutoField(primary_key=True)
+    s_name = models.CharField(max_length=20, verbose_name='姓名', help_text="学生姓名")
+    s_gender = models.CharField(default='男', verbose_name='性别', max_length=6, choices=[('male', '男'), ('female', '女')])
+
+    ID_number = models.IntegerField(null=False)
     native_place = models.CharField(max_length=30)
 
     def __str__(self):
@@ -23,10 +22,8 @@ class Student(models.Model):
 
 
 class Lesson(models.Model):
-    l_number = models.IntegerField(primary_key=True)
-    l_name = models.CharField(max_length=30)
-    credit = models.CharField(max_length=6)
-    time = models.IntegerField()
+    l_number = models.AutoField(primary_key=True)
+    l_name = models.CharField(unique=True, max_length=30, verbose_name="课程")
 
     def __str__(self):
         return self.l_name
@@ -37,11 +34,9 @@ class Lesson(models.Model):
 
 
 class Teacher(models.Model):
-    t_number = models.IntegerField(primary_key=True)
-    t_name = models.CharField(max_length=10)
-    t_pass = models.IntegerField(max_length=6, null=True)
-    t_lesson_id = models.IntegerField(max_length=10)
-    t_lesson_name = models.CharField(max_length=10)
+    t_number = models.AutoField(primary_key=True)
+    t_name = models.CharField(max_length=10, verbose_name="姓名")
+    t_gender = models.CharField(default='男', verbose_name='性别', max_length=6, choices=[('male', '男'), ('female', '女')])
 
     def __str__(self):
         return self.t_name
@@ -52,17 +47,32 @@ class Teacher(models.Model):
 
 
 class Score(models.Model):
-    sName = models.CharField(max_length=10)
-    sNum = models.IntegerField(max_length=15)
-    lName = models.CharField(max_length=10)
-    lNum = models.IntegerField(max_length=10)
-    score = models.IntegerField(max_length=3)
-    lCredit = models.CharField(max_length=6)
-    lTime = models.IntegerField(max_length=5)
+    s_id = models.AutoField(primary_key=True)
+    s_lesson = models.ForeignKey(Lesson, verbose_name="课程", default="", on_delete=models.CASCADE)
+    s_student = models.ForeignKey(Student, verbose_name="学生", default="", on_delete=models.CASCADE)
+    s_score = models.IntegerField(default=0, verbose_name="分数")
 
     def __str__(self):
-        return self.sName
+        return self.s_score
 
     class Meta:
         verbose_name = "成绩"
         verbose_name_plural = "成绩管理"
+
+
+class Teaching(models.Model):
+    t_lesson = models.ForeignKey(Lesson, verbose_name="课程", default="", on_delete=models.CASCADE)
+    t_teacher = models.ForeignKey(Teacher, verbose_name="任课教师", default="", on_delete=models.CASCADE, unique=True)
+
+    class Meta:
+        verbose_name = "教学"
+        verbose_name_plural = "教学管理"
+
+
+class Account(models.Model):
+    a_student = models.ForeignKey(Student, verbose_name="学生", default="", on_delete=models.CASCADE)
+    a_password = models.CharField(verbose_name="密码", default="123456", max_length=20)
+
+    class Meta:
+        verbose_name = "账户"
+        verbose_name_plural = "账户管理"
